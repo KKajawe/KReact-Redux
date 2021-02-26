c0595c0fd4478180ec45bdaf0e6dc1cc32e28ee4
Kalyani Kajawe <kajawek@gmail.com>
2021-02-26 14:58:47 +0100

    Issue:React-Redux with multiple reducer #13 steps 1. create icecream related actions , reducer in redux folder 2. create rootReducer by combining all reducers from redux folder in rootReducer.js 3. pass this rootReducer in store created in store.js file 4. create IceCreamContainer functional component in component folder 5. access redux state property in container taking refernce to its reducer type from rootReducer file 6. add IceCreamContainer in app.js

---
+++ src/components/IceCreamContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { buyIceCream } from '../redux'
+
+function IceCreamContainer(props) {
+    return (
+        <div>
+            <h2>Number of iceCreams - {props.numOfIceCream}</h2>
+            <button onClick={props.buy_icecream}>Buy IceCream</button>
+        </div>
+    )
+}
+
+const mapStateToProps = (state)=>{
+    return{
+        numOfIceCream : state.iceCream.numOfIceCream
+    }
+}
+
+const mapDispatchToProps =(dispatch)=>{
+    return {
+        buy_icecream : ()=>{dispatch(buyIceCream())}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(IceCreamContainer)
